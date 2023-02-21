2b9706e427fda9403c34b9e486491daeab97b53a
Whipstickgostop <8366326+Whipstickgostop@users.noreply.github.com>
2023-02-21 14:08:12 -0500

    initial commit

---
+++ app.config.ts
@@ -0,0 +1,19 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Speaker.bot',
+    description: 'Supercharged Text to Speech (TTS) for your live stream!',
+    image: '/og-image.png',
+    aside: {
+      level: 1,
+      exclude: []
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {
+      credits: {}
+    }
+  }
+})
---
+++ components/Logo.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="logo">
+    <img src="https://streamer.bot/twitchspeaker/logo.svg" width="26" />
+    <span>Speaker.bot <span>Docs</span></span>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+}
+
+.logo > span {
+  font-weight: var(--fontWeight-light);
+  font-size: var(--fontSize-xl);
+  color: var(--color-gray-300);
+  margin-left: .5rem;
+}
+
+.logo > span > span {
+  font-weight: var(--fontWeight-normal);
+  color: var(--color-gray-100);
+}
+</style>
